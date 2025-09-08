cc37db88804607690e0e873496ad496f3fb60204
yoonjuho <dbswn990@naver.com>
2025-09-08 23:31:57 +0900

    hotfix: 누락 응답값

--- src/main/java/be/dash/dashserver/api/core/member/MyLessonDetailedResponse.java
+++ src/main/java/be/dash/dashserver/api/core/member/MyLessonDetailedResponse.java
@@ -5,17 +5,22 @@ import java.util.List;
 import java.util.stream.IntStream;
 import be.dash.dashserver.api.core.member.dto.ApplyStatus;
 import be.dash.dashserver.api.core.member.dto.MemberReservationResponse;
+import be.dash.dashserver.api.core.member.dto.RoundResponse;
 import be.dash.dashserver.core.domain.common.Genre;
 import be.dash.dashserver.core.domain.common.Level;
 import be.dash.dashserver.core.domain.lesson.Lesson;
 import be.dash.dashserver.core.domain.member.Member;
+import be.dash.dashserver.core.domain.reservation.Reservation;
 import be.dash.dashserver.core.domain.reservation.Reservations;
+
+import static java.util.stream.Collectors.toList;
 
 public record MyLessonDetailedResponse(long id,
                                        String name,
                                        String imageUrl,
                                        Genre genre,
                                        Level level,
+                                       List<RoundResponse> rounds,
                                        String location,
                                        String detailedAddress,
                                        LocalDateTime startDateTime,
@@ -31,15 +36,14 @@ public record MyLessonDetailedResponse(long id,
                 lesson.getRepresentativeImageUrl(),
                 lesson.getGenre(),
                 lesson.getLevel(),
+                lesson.getRounds().getRounds().stream().map(RoundResponse::from).collect(toList()),
                 lesson.getLocationName(),
                 lesson.getDetailedAddress(),
                 lesson.getRounds().getStartTime(),
                 lesson.getRounds().getEndTime(),
                 ApplyStatus.calculate(lesson.getStartTime(), lesson.getReservationCount(), lesson.getMaxReservationCount()),
                 IntStream.range(0, members.size())
-                        .mapToObj(i -> MemberReservationResponse.from(members.get(i), reservations.getCreatedAt()
-                                .get(i), reservations.getReservationIds().get(i)))
-                        .toList(),
+                        .mapToObj(i -> MemberReservationResponse.from(members.get(i), reservations.getReservations().get(i))).toList(),
                 members.size()
         );
     }
--- src/main/java/be/dash/dashserver/api/core/member/dto/MemberReservationResponse.java
+++ src/main/java/be/dash/dashserver/api/core/member/dto/MemberReservationResponse.java
@@ -2,12 +2,15 @@ package be.dash.dashserver.api.core.member.dto;
 
 import java.time.LocalDateTime;
 import be.dash.dashserver.core.domain.member.Member;
+import be.dash.dashserver.core.domain.reservation.Reservation;
+import be.dash.dashserver.core.domain.reservation.ReservationStatus;
 
 public record MemberReservationResponse(long reservationId,
                                         String name,
                                         String phoneNumber,
-                                        LocalDateTime createdAt) {
-    public static MemberReservationResponse from(Member member, LocalDateTime createdAt, long reservationId) {
-        return new MemberReservationResponse(reservationId, member.getName(), member.getPhoneNumber(), createdAt);
+                                        LocalDateTime reservationDateTime,
+                                        ReservationStatus reservationStatus) {
+    public static MemberReservationResponse from(Member member, Reservation reservation) {
+        return new MemberReservationResponse(reservation.getId(), member.getName(), member.getPhoneNumber(), reservation.getCreatedAt(), reservation.getReservationStatus());
     }
 }
