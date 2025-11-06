47bb9dfcec26302c09cb220ec28f62000b4d1f71
yoonjuho <dbswn990@naver.com>
2025-11-06 18:44:30 +0900

    feat: swagger 인증 방식 수정

--- src/main/java/be/dash/dashserver/api/config/OpenApiConfig.java
+++ src/main/java/be/dash/dashserver/api/config/OpenApiConfig.java
@@ -1,72 +1,71 @@
 package be.dash.dashserver.api.config;
 
 import java.util.Arrays;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springdoc.core.models.GroupedOpenApi;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.security.SecurityScheme.In;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
 
-	@Bean
-	public OpenAPI customOpenAPI() {
-		return new OpenAPI()
-				.servers(Arrays.asList(
-						new Server().url("https://da-sh.kr").description("Development Server URL"),
-						new Server().url("http://localhost:8080").description("Local Server URL")
-				))
-				.addSecurityItem(new SecurityRequirement().addList("Auth"))
-				.components(attachBearerAuthScheme());
-	}
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(Arrays.asList(
+                        new Server().url("https://dev.da-sh.kr").description("Development Server URL"),
+                        new Server().url("http://localhost:8080").description("Local Server URL")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("Auth"))
+                .components(attachBearerAuthScheme());
+    }
 
-	private Components attachBearerAuthScheme() {
-		return new Components().addSecuritySchemes("Auth",
-				new SecurityScheme()
-						.name("Authorization")
-						.type(Type.HTTP)
-						.scheme("bearer")
-						.bearerFormat("JWT")
-						.description("Use 'Authorization: Bearer <token>' header"));
-	}
+    private Components attachBearerAuthScheme() {
+        return new Components().addSecuritySchemes("Auth",
+                new SecurityScheme()
+                        .name("Authorization")
+                        .type(Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .description("Use 'Authorization: Bearer <token>' header"));
+    }
 
-	@Bean
-	public GroupedOpenApi v1Api() {
-		return GroupedOpenApi.builder()
-				.group("V1 API")
-				.pathsToExclude("/v2/**", "/v3/**", "/v4/**")
-				.build();
-	}
+    @Bean
+    public GroupedOpenApi v1Api() {
+        return GroupedOpenApi.builder()
+                .group("V1 API")
+                .pathsToExclude("/v2/**", "/v3/**", "/v4/**")
+                .build();
+    }
 
-	@Bean
-	public GroupedOpenApi v2Api() {
-		return GroupedOpenApi.builder()
-				.group("V2 API")
-				.pathsToMatch("/v2/**")
-				.build();
-	}
+    @Bean
+    public GroupedOpenApi v2Api() {
+        return GroupedOpenApi.builder()
+                .group("V2 API")
+                .pathsToMatch("/v2/**")
+                .build();
+    }
 
-	@Bean
-	public GroupedOpenApi v3Api() {
-		return GroupedOpenApi.builder()
-				.group("V3 API")
-				.pathsToMatch("/v3/**")
-				.build();
-	}
+    @Bean
+    public GroupedOpenApi v3Api() {
+        return GroupedOpenApi.builder()
+                .group("V3 API")
+                .pathsToMatch("/v3/**")
+                .build();
+    }
 
-	@Bean
-	public GroupedOpenApi v4Api() {
-		return GroupedOpenApi.builder()
-				.group("V4 API")
-				.pathsToMatch("/v4/**")
-				.build();
-	}
+    @Bean
+    public GroupedOpenApi v4Api() {
+        return GroupedOpenApi.builder()
+                .group("V4 API")
+                .pathsToMatch("/v4/**")
+                .build();
+    }
 }
 
 
