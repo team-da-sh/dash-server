fd293d57307aca85469a24ae82893f689a779c87
박희선 <heeseon.park@aicx.kr>
2025-12-30 23:38:48 +0900

    fix: reservationDateTime 필드에만 Asia/Seoul 타임존 적용

--- src/main/java/be/dash/dashserver/api/core/member/dto/MemberReservationResponse.java
+++ src/main/java/be/dash/dashserver/api/core/member/dto/MemberReservationResponse.java
@@ -4,10 +4,12 @@ import java.time.LocalDateTime;
 import be.dash.dashserver.core.domain.member.Member;
 import be.dash.dashserver.core.domain.reservation.Reservation;
 import be.dash.dashserver.core.domain.reservation.ReservationStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public record MemberReservationResponse(long reservationId,
                                         String name,
                                         String phoneNumber,
+                                        @JsonFormat(timezone = "Asia/Seoul")
                                         LocalDateTime reservationDateTime,
                                         ReservationStatus reservationStatus) {
     public static MemberReservationResponse fromApprove(Member member, Reservation reservation) {
