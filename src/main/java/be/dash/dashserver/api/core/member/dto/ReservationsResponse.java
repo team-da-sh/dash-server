9efe8031eeb8860781e5f2aeca7c99c51a6368a3
yoonjuho <dbswn990@naver.com>
2026-01-01 23:07:39 +0900

    feat: 클래스 목록 신청 오래된순으로 정렬 변경

--- src/main/java/be/dash/dashserver/api/core/member/dto/ReservationsResponse.java
+++ src/main/java/be/dash/dashserver/api/core/member/dto/ReservationsResponse.java
@@ -1,5 +1,6 @@
 package be.dash.dashserver.api.core.member.dto;
 
+import java.util.Comparator;
 import java.util.List;
 import be.dash.dashserver.core.domain.member.service.ReservationResult;
 
@@ -7,6 +8,7 @@ public record ReservationsResponse(List<ReservationResponse> reservations) {
     public static ReservationsResponse from(List<ReservationResult> reservations) {
         return new ReservationsResponse(
                 reservations.stream()
+                        .sorted(Comparator.comparing(ReservationResult::createdAt))
                         .map(ReservationResponse::from)
                         .toList()
         );
